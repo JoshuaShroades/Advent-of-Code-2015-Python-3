ccabf1ccfeb56a619e398e558144b763dd17af06
Joshua Shroades <167574622+Joshua-Shroades@users.noreply.github.com>
2024-04-20 16:11:51 -0400

    Day 3 Part 1

---
+++ Day 03/part1.py
@@ -0,0 +1,25 @@
+import os
+
+fileDirectory = os.path.dirname(os.path.realpath(__file__))
+inputFilePath = os.path.join(fileDirectory, "input.txt")
+
+with open(inputFilePath) as inputFile:
+	line = inputFile.readline()
+
+xPos = 0
+yPos = 0
+visitedCoordinates = {(0,0)}
+
+for char in line:
+	match char:
+		case "^":
+			xPos += 1
+		case "v":
+			xPos -= 1
+		case ">":
+			yPos += 1
+		case "<":
+			yPos -= 1
+	visitedCoordinates.add((xPos, yPos))
+
+print(len(visitedCoordinates))
