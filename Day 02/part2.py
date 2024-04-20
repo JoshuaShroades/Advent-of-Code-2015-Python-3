d851303e605f729424ddb81749c071127c180c20
Joshua Shroades <167574622+Joshua-Shroades@users.noreply.github.com>
2024-04-20 14:22:38 -0400

    Day 2 Part 2

---
+++ Day 02/part2.py
@@ -0,0 +1,23 @@
+import os
+
+fileDirectory = os.path.dirname(os.path.realpath(__file__))
+inputFilePath = os.path.join(fileDirectory, "input.txt")
+
+with open(inputFilePath) as inputFile:
+	boxDimensions = [line[:-1] for line in inputFile]
+
+totalLength = 0
+
+for box in boxDimensions:
+	dimensions = [int(dimension) for dimension in box.split("x")]
+
+	length = dimensions[0]
+	width = dimensions[1]
+	height = dimensions[2]
+
+	ribbonLength = 2 * (sum(dimensions) - max(dimensions))
+	bowLength = length * width * height
+
+	totalLength += ribbonLength + bowLength
+
+print(totalLength)
