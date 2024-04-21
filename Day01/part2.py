416919a917c5bdf17f44c9742f46b2c071e3b70a
Joshua Shroades <167574622+Joshua-Shroades@users.noreply.github.com>
2024-04-21 13:29:44 -0400

    Update part2.py

--- Day01/part2.py
+++ Day01/part2.py
@@ -5,10 +5,11 @@ inputFilePath = os.path.join(fileDirectory, "input.txt")
 
 with open(inputFilePath) as inputFile:
 	line = inputFile.readline()
-	for char in range(len(line)):
-		upCount = line[:char].count("(")
-		downCount = line[:char].count(")")
-		finalFloor = upCount - downCount
-		if(finalFloor == -1):
-			print(char)
-			break
+
+for char in range(len(line)):
+	upCount = line[:char].count("(")
+	downCount = line[:char].count(")")
+	finalFloor = upCount - downCount
+	if(finalFloor == -1):
+		print(char)
+		break
